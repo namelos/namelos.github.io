908da6be82c81b82f001329a5183ff4eedea17a4
namelos <namelos@outlook.com>
2018-09-22 10:27:28 +0800

    make simple org blog

---
+++ gatsby-config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  pathPrefix: "/gatsby-orga",
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `pages`,
+        path: `${__dirname}/src/pages/`,
+      },
+    },
+    {
+      resolve: `gatsby-transformer-orga`
+    }
+  ]
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,36 @@
-import React from "react"
+import React from 'react'
+import Link from 'gatsby-link'
 
-export default () => <div>Hello world!</div>
+export default function BlogIndex({ data }) {
+  return <div>
+    <h1>Blog</h1>
+    {data.allOrga.edges.map(({ node }) =>
+      <Post {...node} key={node.fields.slug} />)}
+  </div>
+}
+
+function Post({ meta, fields }) {
+  const title = meta.title || fields.slug
+  const date = meta.date || 'no date'
+  return <div>
+    <h3>
+      <Link to={fields.slug}>{title}</Link>
+    </h3>
+    <small>{date}</small>
+  </div>
+}
+
+export const pageQuery = graphql`
+  query IndexQuery {
+    allOrga {
+      edges {
+        node {
+          fields {
+            slug
+          }
+          meta
+        }
+      }
+    }
+  }
+`
